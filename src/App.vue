d51170201dac4a6e72b5173b6183128612a9064c
everglow <78077569+okchanho@users.noreply.github.com>
2021-07-04 22:13:21 +0900

    7/4

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,11 @@
     </div>
 
     <Container :feedData="feedData" />
+    <div class="moreFeed">
+      <button @click="more">더보기</button>
+    </div>
+
+
 
     <div class="footer">
       <ul class="footer-button-plus">
@@ -25,7 +30,8 @@
 
 <script>
 import Container from './components/Container.vue';
-import feedDatd from './assets/js/feedData.js';
+import feedData from './assets/js/feedData.js';
+import axios from 'axios';
 
 
 
@@ -33,12 +39,31 @@ export default {
   name: "App",
   data() {
     return {
-      feedData : feedDatd,
+      feedData : feedData,
+      morePointer : 0,
     }
   },
   components: {
     Container
   },
+  methods : {
+    more(){
+
+      // axios.post('https://codingapple1.github.io/vue/more0.json')
+      // .then()
+      // .catch((err)=>{
+      //   console.log(err)
+      // });
+      
+      axios.get(`https://codingapple1.github.io/vue/more`+`${this.morePointer}`+`.json`)
+      .then( moreData =>{
+        console.log(moreData.data);
+        this.morePointer++;
+        console.log(this.morePointer);
+        this.feedData.push(moreData.data);
+      })
+    }
+  }
 };
 </script>
 
