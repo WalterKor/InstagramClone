940d118f3a2c55ad27a8374492209dc6e318b945
everglow <78077569+okchanho@users.noreply.github.com>
2021-07-04 19:38:57 +0900

    7/5

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+
+  <div>
+
+    <div class="header">
+      <ul class="header-button-left">
+        <li>Cancel</li>
+      </ul>
+      <ul class="header-button-right">
+        <li>Next</li>
+      </ul>
+      <img src="./assets/logo.png" class="logo" />
+    </div>
+
+    <Container />
+
+    <div class="footer">
+      <ul class="footer-button-plus">
+        <input type="file" id="file" class="inputfile" />
+        <label for="file" class="input-plus">+</label>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import Container from './components/Container.vue';
+
+
+export default {
+  name: "App",
+  components: {
+    Container
+  },
+};
+</script>
+
+<style>
+  @import url(./assets/css/app.css);
+</style>
---
+++ src/components/Container.vue
@@ -0,0 +1,29 @@
+<template>
+     <div>
+        <Post/>
+        <Post/>
+        <Post/>
+    </div>
+</template>
+
+<script>
+import Post from './Post.vue';
+
+export default {
+    
+    data() {
+        return {
+            
+        }
+    },
+
+    components: {
+        Post : Post
+    }
+
+}
+</script>
+
+<style>
+
+</style>
