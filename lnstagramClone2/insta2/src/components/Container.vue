e8ce1a1004daded41672abbfa6fbc7a4665c4d1d
everglow <78077569+okchanho@users.noreply.github.com>
2021-07-08 10:03:38 +0900

    7/7

---
+++ lnstagramClone2/insta2/src/components/Container.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Post :feedData[0]= "feedData"/>
+    <Post/>
+    <Post/>
+</div>
+</template>
+
+<script>
+import Post from './Post.vue';
+
+export default {
+  components : {
+    Post
+  },
+  props : {
+    feedData : Array
+  }
+}
+</script>
+
+<style>
+
+</style>
